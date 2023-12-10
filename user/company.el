2e4f5fc88441cf2b6c64468950443dcefae185bd
max <maximilian.filipov@stud.fra-uas.de>
2023-12-10 03:06:53 +0100

    f

--- user/company.el
+++ user/company.el
@@ -1,8 +1,10 @@
 ;;; user/company.el -*- lexical-binding: t; -*-
 
-
-
 (after! company
-  (setq company-dabbrev-downcase 2)
-  (setq company-idle-delay 0.1)
-  (setq company-minimum-prefix-length 2))
+  (setq company-dabbrev-downcase 1)
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1)
+  (define-key company-active-map (kbd "<tab>") #'company-complete-selection)
+  (define-key company-active-map (kbd "<return>") nil)
+  (define-key company-active-map (kbd "RET") nil)
+  (global-company-mode))
